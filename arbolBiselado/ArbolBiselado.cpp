f5b2bed3028dc65736d99d5336b7675aded11855
MarianoCinalli <mariano_cinalli@hotmail.com>
2016-05-14 17:46:23 -0300

    Agrego la estructura de la clase ArbolBiselado

---
+++ arbolBiselado/ArbolBiselado.cpp
@@ -0,0 +1,36 @@
+#include "ArbolBiselado.h"
+
+class ArbolBiselado{
+
+	// Atributos
+
+	private:
+	int numeroDeBloqueHijoIzquierdo;
+	int numeroDeBloqueHijoDerecho;
+	Nodo* hijoIzquierdo;
+	Nodo* hijoDerecho;
+	Lista* registros;
+
+	// Metodos
+
+	public:
+	ArbolBiselado();
+
+	void setNumeroDeBloqueHijoIzquierdo(int numero);
+	int getNumeroDeBloqueHijoIzquierdo();
+
+	void setNumeroDeBloqueHijoDerecho(int numero);
+	int getNumeroDeBloqueHijoDerecho();
+
+	void setHijoIzquierdo(Nodo* hijoIzquierdo);
+	Nodo* getHijoIzquierdo();
+
+	void setHijoDerecho(Nodo* hijoDerecho);
+	Nodo* getHijoDerecho();
+	
+	void insertar(Registro* registro);
+	void modificar(Registro* registro);
+	void eliminar(int identificador);
+
+	~ArbolBiselado();
+};
---
+++ arbolBiselado/ArbolBiselado.h
@@ -0,0 +1,38 @@
+#ifndef _ARBOLBISELADO_H
+#define _ARBOLBISELADO_H
+
+class ArbolBiselado{
+
+	// Atributos
+
+	private:
+	int numeroDeBloqueHijoIzquierdo;
+	int numeroDeBloqueHijoDerecho;
+	Nodo* hijoIzquierdo;
+	Nodo* hijoDerecho;
+	Lista* registros;
+
+	// Metodos
+	public:
+	ArbolBiselado();
+
+	void setNumeroDeBloqueHijoIzquierdo(int numero);
+	int getNumeroDeBloqueHijoIzquierdo();
+
+	void setNumeroDeBloqueHijoDerecho(int numero);
+	int getNumeroDeBloqueHijoDerecho();
+
+	void setHijoIzquierdo(Nodo* hijoIzquierdo);
+	Nodo* getHijoIzquierdo();
+
+	void setHijoDerecho(Nodo* hijoDerecho);
+	Nodo* getHijoDerecho();
+	
+	void insertar(Registro* registro);
+	void modificar(Registro* registro);
+	void eliminar(int identificador);
+
+	~ArbolBiselado();
+};
+
+#endif // ARBOLBISELADO_H
