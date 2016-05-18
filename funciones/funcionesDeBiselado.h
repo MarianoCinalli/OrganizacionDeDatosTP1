b6224cf6c229808f946503d3d71b988614c6cca6
rayoazul <rayoazul@192.168.1.2>
2016-05-18 16:25:15 -0300

    Pusheo para probar
    Para ver si sale el nombre de mi usuario

--- funciones/funcionesDeBiselado.h
+++ funciones/funcionesDeBiselado.h
@@ -1,6 +1,7 @@
 #ifndef BISELAR_INCLUDED
 #define BISELAR_INCLUDED
 #include "C:/CSV/TpDatos/OrganizacionDeDatosTP1/estructuras/pila.h"
+//Prueba
 Pila<Nodo>* biselar( Pila<Nodo>* nodos );
 
 #endif // BISELAR_INCLUDED
